61f40d9e5ad951f3da6a35e87ffc0a277eadca32
nexaafritech71-afk <nexaafritech71@gmail.com>
2026-08-08 15:06:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Bienvenue sur le site Nexa AfriTech");
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
+});
+
+const links = navLinks.querySelectorAll("a");
+
+links.forEach(function (link) {
+    link.addEventListener("click", function () {
+        navLinks.classList.remove("show");
+    });
+});
